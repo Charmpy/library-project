2b823efd478d830c013deeb20a7ba4a2ef3fd565
Charmpy <alex-cvc@mail.ru>
2020-11-14 18:34:03 +0500

    14.11.2020 BookWidget class + DB requests

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 import sqlite3
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QWidget, QDialog
+from PyQt5.QtWidgets import QApplication, QWidget, QDialog, QMessageBox
 from PyQt5.QtWidgets import QStackedWidget, QVBoxLayout, QTableWidgetItem, QHeaderView, QTabWidget
 
 
@@ -15,6 +15,18 @@ class Data:
         SELECT * FROM books
         WHERE id > 0
         ''').fetchall()
+        return [list(i) for i in rez]
+
+    def get_authors(self):
+        rez = self.cur.execute('''
+            SELECT * FROM authors
+        ''',).fetchall()
+        return [list(i) for i in rez]
+
+    def get_genres(self):
+        rez = self.cur.execute('''
+            SELECT * FROM genres
+        ''',).fetchall()
         return [list(i) for i in rez]
 
     def get_bookinfo(self, index):
@@ -54,6 +66,12 @@ class Data:
             ''').fetchall()
         return [list(i) for i in rez]
 
+    def add_book(self, title, author, genre, year):
+        ide = self.cur.execute("select max(id) from books").fetchall()[0][0] + 1
+        self.cur.execute('''
+                            INSERT INTO books(id, title, author, genre, year) VALUES(?, ?, ?, ?, ?) 
+                            ''', (ide, title, author, genre, year, ))
+
 
 class Main(QWidget):
     def __init__(self):
@@ -90,11 +108,12 @@ class BookWidget(QWidget):
     def initUI(self):
         uic.loadUi('book_widget.ui', self)
         self.back.clicked.connect(lambda x: self.root.widget.setCurrentIndex(0))
-        self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
-        head = ['id', 'Название', 'Автор', 'Жанр', 'Год']
-        info = self.root.data.get_books('123')
-        self.change_info(info)
-        show_tbl(self.table, info, head)
+
+        self.book_tbl.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.author_tbl.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.genre_tbl.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.refresh_tables()
+
         self.add_book_btn.clicked.connect(self.add_book_dialog)
         self.add_author_btn.clicked.connect(self.add_author_dialog)
         self.add_genre_btn.clicked.connect(self.add_genre_dialog)
@@ -103,7 +122,21 @@ class BookWidget(QWidget):
         self.change_author_btn.clicked.connect(self.change_author_dialog)
         self.change_genre_btn.clicked.connect(self.change_genre_dialog)
 
-    def change_info(self, info):
+    def refresh_tables(self):
+        book_head = ['id', 'Название', 'Автор', 'Жанр', 'Год']
+        book_list = self.root.data.get_books('123')
+        self.change_book_list(book_list)
+        show_tbl(self.book_tbl, book_list, book_head)
+
+        author_head = ['id', 'ФИО']
+        author_list = self.root.data.get_authors()
+        show_tbl(self.author_tbl, author_list, author_head)
+
+        genre_head = ['id', 'ФИО']
+        genre_list = self.root.data.get_genres()
+        show_tbl(self.genre_tbl, genre_list, genre_head)
+
+    def change_book_list(self, info):
         for i in info:
             i[2] = self.root.data.get_authorname(i[2])
             i[3] = self.root.data.get_genrename(i[3])
@@ -113,10 +146,33 @@ class BookWidget(QWidget):
             self.d.close()
 
         def check():
-            self.d.close()
+            t = self.d.title_in.text()
+            a = self.d.author_combo.currentIndex() + 1
+            g = self.d.genre_combo.currentIndex() + 1
+            y = self.d.year_in.text()
+            msg = QMessageBox(self.d)
+            if bool(t) and bool(y) and bool(a) and bool(g):
+                try:
+                    y = int(y)
+                except ValueError:
+                    msg.question(self, '', 'Неверный год', msg.Ok)
+
+                else:
+                    if 0 < y <= 2020:
+                        self.root.data.add_book(t, a, g, y)
+                        self.refresh_tables()
+                        self.d.close()
+                    else:
+                        msg.question(self, '', 'Неверный год', msg.Ok)
+            else:
+                msg.question(self, '', 'Не все поля заполнены', msg.Ok)
 
         self.d = QDialog()
         uic.loadUi('book_add_dialog.ui', self.d)
+        authors = [i[1] for i in self.root.data.get_authors()]
+        genres = [i[1] for i in self.root.data.get_genres()]
+        self.d.author_combo.addItems(authors)
+        self.d.genre_combo.addItems(genres)
         self.d.show()
         self.d.cancel_btn.clicked.connect(close)
         self.d.ok_btn.clicked.connect(check)
