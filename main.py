eed6a93c6f142fa2626c40f058441ec0e9b226f0
Charmpy <alex-cvc@mail.ru>
2020-11-17 20:58:24 +0500

    17.11.2020 BookWidget(ready) GiveWidget(ready)

--- main.py
+++ main.py
@@ -185,12 +185,19 @@ class Data:
         DELETE FROM genres
         WHERE id = ?''', (index, ))
 
-    def find_users(self, line, flag=True):
-        if flag:
+    def find_users(self, line, flag=''):
+        if flag == 1:
             rez = self.cur.execute(
                 '''
                 SELECT * FROM users
                 WHERE name LIKE ? AND books = 0
+                ''', (f'%{line}%',)).fetchall()
+
+        elif flag == 2:
+            rez = self.cur.execute(
+                '''
+                SELECT * FROM users
+                WHERE name LIKE ? AND books > 0
                 ''', (f'%{line}%',)).fetchall()
         else:
             rez = self.cur.execute(
@@ -199,6 +206,37 @@ class Data:
                 WHERE name LIKE ?
                 ''', (f'%{line}%',)).fetchall()
         return [list(i) for i in rez]
+
+    def check_book(self, book_author, book_title):
+        rez = self.cur.execute(
+            '''
+            SELECT id FROM users
+            WHERE books = (
+                SELECT id FROM books WHERE title LIKE ? AND author = (SELECT id FROM authors WHERE name LIKE ?)
+            )
+            ''', (book_title, book_author,)).fetchall()
+        if rez:
+            return True
+        return False
+
+    def give_book(self, user_name, book_author, book_title):
+        self.cur.execute(
+            '''
+            UPDATE users
+            SET books = (
+                SELECT id FROM books 
+                WHERE title LIKE ? AND author = (SELECT id FROM authors WHERE name LIKE ?)
+                )
+            WHERE name = ?
+            ''', (book_title, book_author, user_name, ))
+
+    def back_book(self, user_name):
+        self.cur.execute(
+            '''
+            UPDATE users
+            SET books = 0
+            WHERE name LIKE ?
+            ''', (user_name, ))
 
 
 class Main(QWidget):
@@ -506,11 +544,8 @@ class GiveWidget(QWidget):
     def initUI(self):
         uic.loadUi('give_widget.ui', self)
         self.back.clicked.connect(lambda x: self.root.widget.setCurrentIndex(0))
-        self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
-        head = ['id', 'ФИО', 'Книга']
-        info = self.root.data.get_readers(True)
-        self.change_info(info)
-        show_tbl(self.table, info, head)
+        self.table.horizontalHeader().setStretchLastSection(True)
+        self.refresh_table()
         self.give_btn.clicked.connect(self.give_dialog)
         self.get_btn.clicked.connect(self.back_dialog)
 
@@ -519,34 +554,93 @@ class GiveWidget(QWidget):
             i[-1] = self.root.data.get_bookinfo(i[-1])
 
     def give_dialog(self):
+        def refresh(line):
+            info = self.root.data.find_users(line, '1')
+            show_tbl(self.d.name_tbl, info, head)
+
+        def chose_user(x, y):
+            info = [self.d.name_tbl.item(x, 0).text(), self.d.name_tbl.item(x, 1).text()]
+            self.d.name_tbl.setDisabled(True)
+            self.d.user_name.setText(info[1])
+
+        def change(line):
+            self.d.title_combo.clear()
+            self.d.title_combo.setDisabled(False)
+            titles = [i[1] for i in self.root.data.get_books(2, line)]
+            self.d.title_combo.addItems(titles)
+
         def close():
             self.d.close()
 
         def check():
-            self.d.close()
+            n = self.d.user_name.text()
+            a = self.d.author_combo.currentText()
+            t = self.d.title_combo.currentText()
+            if bool(n) and bool(a) and bool(t):
+                flag = self.root.data.check_book(a, t)
+                if flag:
+                    self.d.error.setText('Данная книга уже выдана')
+                else:
+                    self.root.data.give_book(n, a, t)
+                    self.d.close()
+                    self.refresh_table()
+            else:
+                self.d.error.setText('Не все поля заполнены')
 
         self.d = QDialog()
         uic.loadUi('give_give_dialog.ui', self.d)
         self.d.name_tbl.horizontalHeader().setStretchLastSection(True)
         head = ['id', 'ФИО']
-        info = self.root.data.find_users('', True)
+        info = self.root.data.find_users('', 1)
         show_tbl(self.d.name_tbl, info, head)
+        authors = [i[1] for i in self.root.data.get_authors()]
+        self.d.author_combo.addItems(authors)
+
+        self.d.author_combo.currentTextChanged.connect(change)
+        self.d.title_combo.setDisabled(True)
+
         self.d.show()
         self.d.cancel_btn.clicked.connect(close)
         self.d.ok_btn.clicked.connect(check)
+        self.d.name_tbl.cellClicked.connect(chose_user)
+        self.d.name_in.textChanged.connect(refresh)
+
+    def refresh_table(self):
+        head = ['id', 'ФИО', 'Книга']
+        info = self.root.data.get_readers(True)
+        self.change_info(info)
+        show_tbl(self.table, info, head)
 
     def back_dialog(self):
         def close():
             self.d.close()
 
+        def chose_user(x, y):
+            self.out_info = [self.d.name_tbl.item(x, i).text() for i in range(3)]
+            self.d.user_name.setText(self.out_info[1])
+            self.d.book_title.setText(self.out_info[2])
+
         def check():
-            self.d.close()
+            n = self.d.user_name.text()
+            if bool(n):
+                self.root.data.back_book(n)
+                self.refresh_table()
+                self.d.close()
+            else:
+                self.d.error.setText('Выберите пользователя')
 
         self.d = QDialog()
         uic.loadUi('give_back_dialog.ui', self.d)
+        head = ['id', 'ФИО', 'книга']
+        info = self.root.data.get_readers(True)
+        self.change_info(info)
+        self.d.name_tbl.horizontalHeader().setStretchLastSection(True)
+        show_tbl(self.d.name_tbl, info, head)
+
         self.d.show()
         self.d.cancel_btn.clicked.connect(close)
         self.d.ok_btn.clicked.connect(check)
+        self.d.name_tbl.cellClicked.connect(chose_user)
 
 
 class UsersWidget(QWidget):
